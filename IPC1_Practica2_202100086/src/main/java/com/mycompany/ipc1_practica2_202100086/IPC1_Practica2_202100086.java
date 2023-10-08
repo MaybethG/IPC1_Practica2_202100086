122acbf6c0fdf212741e2b7a481d39dfb126744e
MaybethG <140296693+MaybethG@users.noreply.github.com>
2023-10-08 12:49:34 -0600

    Creacion del proyecto
    Creacion del proyecto en NetBeans

---
+++ IPC1_Practica2_202100086/src/main/java/com/mycompany/ipc1_practica2_202100086/IPC1_Practica2_202100086.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ipc1_practica2_202100086;
+
+/**
+ *
+ * @author Fati
+ */
+public class IPC1_Practica2_202100086 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
